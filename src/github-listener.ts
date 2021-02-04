ed4f735f4de8313000a51e44ef32f6b7083cbfc6
ialford <ialford@nd.edu>
2021-02-04 14:00:48 -0500

    feat: get token at runtime?

--- src/github-listener.ts
+++ src/github-listener.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core'
 import { LambdaIntegration, RestApi } from '@aws-cdk/aws-apigateway'
 import { Code, Function, Runtime } from '@aws-cdk/aws-lambda'
+import { StringParameter } from '@aws-cdk/aws-ssm'
 
 export class GitHubListenerStack extends cdk.Stack {
   constructor (scope: cdk.Construct, id: string, props: cdk.StackProps) {
@@ -12,6 +13,9 @@ export class GitHubListenerStack extends cdk.Stack {
       code: Code.fromAsset('lambdas/'),
       runtime: Runtime.NODEJS_12_X,
       handler: 'githubWebhookListener',
+      environment:{
+        GITHUB_WEBHOOK_SECRET: "/all/github-listener/github-token",
+      },
     })
 
     const api = new RestApi(this, 'webhook-listener', {
